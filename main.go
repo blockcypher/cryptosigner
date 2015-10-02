254574e1310abd96affdd1b0152b09e6e247975a
Matthieu Riou <matthieu@offthelip.org>
2015-10-02 10:06:42 -0700

    Fixed local import.

--- main.go
+++ main.go
@@ -1,26 +1,32 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  . "./signer"
+	"fmt"
+	. "github.com/blockcypher/cryptosigner/signer"
+	"log"
 )
 
 func main() {
-  fmt.Print("Enter password: ")
-  var pwd string
-  fmt.Scanln(&pwd)
-  if len(pwd) == 0 {
-    log.Fatal("Could not read.")
-  }
+	fmt.Print("Enter password: ")
+	var pwd string
+	fmt.Scanln(&pwd)
+	if len(pwd) == 0 {
+		log.Fatal("Could not read.")
+	}
 
-  signer := &ECDSASigner{}
-  store, err := MakeFileStore()
-  if err != nil { log.Println(err); return }
+	signer := &ECDSASigner{}
+	store, err := MakeFileStore()
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
-  hold, err := MakeHold(pwd, store, signer)
-  if err != nil { log.Println(err); return }
+	hold, err := MakeHold(pwd, store, signer)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
-  log.Println("Starting server")
-  StartServer(hold)
+	log.Println("Starting server")
+	StartServer(hold)
 }
